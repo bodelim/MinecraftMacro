a0274011b6d236dbbbde412a648a32c2e0dde076
bodelim <darguria@gmail.com>
2021-05-05 19:55:58 +0900

    파일 업로드

---
+++ guiTutorial/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace guiTutorial
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.MiningLimit_Text = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fixCommand_Text = new System.Windows.Forms.TextBox();
+            this.ClickSpeed_Text = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.scanBtn = new System.Windows.Forms.Button();
+            this.FragmentCheckBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(236, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 98);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "변경사항 적용";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("배달의민족 주아", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(3, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "수리 명령어:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(22, 26);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.MiningLimit_Text);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.fixCommand_Text);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint_1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.ClickSpeed_Text);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(188, 324);
+            this.splitContainer1.SplitterDistance = 100;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // MiningLimit_Text
+            // 
+            this.MiningLimit_Text.Location = new System.Drawing.Point(98, 54);
+            this.MiningLimit_Text.Name = "MiningLimit_Text";
+            this.MiningLimit_Text.Size = new System.Drawing.Size(81, 23);
+            this.MiningLimit_Text.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("배달의민족 주아", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(3, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "광질 제한시간:";
+            this.label4.Click += new System.EventHandler(this.label4_Click_1);
+            // 
+            // fixCommand_Text
+            // 
+            this.fixCommand_Text.Location = new System.Drawing.Point(84, 14);
+            this.fixCommand_Text.Name = "fixCommand_Text";
+            this.fixCommand_Text.Size = new System.Drawing.Size(95, 23);
+            this.fixCommand_Text.TabIndex = 2;
+            // 
+            // ClickSpeed_Text
+            // 
+            this.ClickSpeed_Text.Location = new System.Drawing.Point(91, 19);
+            this.ClickSpeed_Text.Name = "ClickSpeed_Text";
+            this.ClickSpeed_Text.Size = new System.Drawing.Size(50, 23);
+            this.ClickSpeed_Text.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("배달의민족 주아", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(3, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "클릭속도(ms):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("배달의민족 주아", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(223, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "이 프로그램은 마인크래프트에서";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("배달의민족 주아", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(22, 371);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(156, 17);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "F8: 자동 곡괭이 시작/중지";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("배달의민족 주아", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(22, 403);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(139, 17);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "F9: 오토클릭 시작/중지";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(407, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(756, 394);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // scanBtn
+            // 
+            this.scanBtn.Location = new System.Drawing.Point(274, 257);
+            this.scanBtn.Name = "scanBtn";
+            this.scanBtn.Size = new System.Drawing.Size(75, 23);
+            this.scanBtn.TabIndex = 12;
+            this.scanBtn.Text = "화면스캔";
+            this.scanBtn.UseVisualStyleBackColor = true;
+            this.scanBtn.Click += new System.EventHandler(this.scanBtn_Click);
+            // 
+            // FragmentCheckBox
+            // 
+            this.FragmentCheckBox.AutoSize = true;
+            this.FragmentCheckBox.Location = new System.Drawing.Point(232, 300);
+            this.FragmentCheckBox.Name = "FragmentCheckBox";
+            this.FragmentCheckBox.Size = new System.Drawing.Size(163, 19);
+            this.FragmentCheckBox.TabIndex = 14;
+            this.FragmentCheckBox.Text = "Fish서버 돌조각 자동생산";
+            this.FragmentCheckBox.UseVisualStyleBackColor = true;
+            this.FragmentCheckBox.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1175, 437);
+            this.Controls.Add(this.FragmentCheckBox);
+            this.Controls.Add(this.scanBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Fish서버 돌조각 자동생산";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox fixCommand_Text;
+        private System.Windows.Forms.TextBox ClickSpeed_Text;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button scanBtn;
+        private System.Windows.Forms.CheckBox FragmentCheckBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox MiningLimit_Text;
+    }
+}
+
---
+++ guiTutorial/Form1.cs
@@ -0,0 +1,441 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace guiTutorial
+{
+
+    public partial class Form1 : Form
+    {
+
+        [System.Runtime.InteropServices.DllImport("User32", EntryPoint = "FindWindow")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        internal static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, int nFlags);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, IntPtr lParam);
+
+        [System.Runtime.InteropServices.DllImport("User32.dll")]
+
+        private static extern IntPtr FindWindowEx(IntPtr hWnd1, int hWnd2, string lp1, string lp2);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc callback, IntPtr hInstance, uint threadId);
+        [DllImport("user32.dll")]
+        static extern bool UnhookWindowsHookEx(IntPtr hInstance);
+        [DllImport("user32.dll")]
+        static extern IntPtr CallNextHookEx(IntPtr idHook, int nCode, int wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+        [DllImport("kernel32.dll")] static extern IntPtr LoadLibrary(string lpFileName);
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        const int WH_KEYBOARD_LL = 13;
+        const int WM_KEYDOWN = 0x100;
+        private LowLevelKeyboardProc _proc = hookProc;
+        private static IntPtr hhook = IntPtr.Zero;
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x0002;      // 좌클릭 누름
+        private const uint MOUSEEVENTF_LEFTUP = 0x0004;        // 좌클릭 뗌
+        const uint MOUSEEVENTF_RBUTTONDOWN = 0x0008;    // 오른쪽 마우스 버튼 눌림
+        const uint MOUSEEVENTF_RBUTTONUP = 0x00010;      // 오른쪽 마우스 버튼 떼어짐
+        public static Boolean leftPower = false;
+
+        String AppPlayerName = "Minecraft 1.16.5 - 멀티플레이 (제삼자 서버)";
+
+        private System.Windows.Forms.Timer scanloopTimer;
+
+        public static System.Windows.Forms.Timer FixToolTimer = new System.Windows.Forms.Timer();
+
+        private static System.Windows.Forms.Timer FastClickTimer;
+
+        public static int count = 0;
+
+        public static int FragmentCount = 0;
+        // 돌 조각으로 변환시킬 카운트
+
+        public String fixCommnad;
+
+        public int ClickSpeed;
+
+        public static Boolean FastClickPower = false;
+
+        public static Boolean FragmentAutoPower = false;
+
+        public int MiningLimit = 200;
+
+        public void SetHook() {
+            IntPtr hInstance = LoadLibrary("User32");
+            hhook = SetWindowsHookEx(WH_KEYBOARD_LL, _proc, hInstance, 0);
+        }
+        public static void UnHook() { 
+            UnhookWindowsHookEx(hhook); 
+        }
+
+      
+        
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            UnHook();
+        }
+        
+    
+        
+        
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "마인크래프트 매크로";
+        }
+        bool isRunning = true;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fixCommnad = fixCommand_Text.Text;
+            MiningLimit = Int32.Parse(MiningLimit_Text.Text);
+            MessageBox.Show("설정이 적용되었습니다.");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void scanLoop(object sender, EventArgs e)
+        {
+
+
+            IntPtr findwindow = FindWindow(null, AppPlayerName);
+
+            IntPtr fwEx = FindWindowEx(findwindow, 0, "GLFW30", "Minecraft 1.16.5 - 멀티플레이 (제삼자 서버)");
+            //1: findwindow 2: 0 3: 클래스명 4: 캡션명
+
+            if (findwindow != IntPtr.Zero)
+            {
+
+                //플레이어를 찾았을 경우
+                Debug.WriteLine("설정한 앱을 찾았습니다.");
+
+                try
+                {
+
+                    //찾은 플레이어를 바탕으로 Graphics 정보를 가져옵니다.
+                    Graphics Graphicsdata = Graphics.FromHwnd(findwindow);
+
+                    //찾은 플레이어 창 크기 및 위치를 가져옵니다. 
+                    Rectangle rect = Rectangle.Round(Graphicsdata.VisibleClipBounds);
+
+                    //플레이어 창 크기 만큼의 비트맵을 선언해줍니다.
+                    Bitmap bmp = new Bitmap(rect.Width, rect.Height);
+
+                    //비트맵을 바탕으로 그래픽스 함수로 선언해줍니다.
+                    using (Graphics g = Graphics.FromImage(bmp))
+                    {
+                        //찾은 플레이어의 크기만큼 화면을 캡쳐합니다.
+                        IntPtr hdc = g.GetHdc();
+                        PrintWindow(fwEx, hdc, 0x2);
+                        g.ReleaseHdc(hdc);
+                    }
+
+                    // pictureBox1 이미지를 표시해줍니다.
+                    pictureBox1.Image = bmp;
+                }
+                catch (Exception)
+                {
+                    Debug.WriteLine("오류");
+                }
+            }
+            else
+            {
+                //마크를 못찾을경우
+                Debug.WriteLine("설정한 앱을 찾지 못했습니다.");
+            }
+        }
+
+        private async void Run()
+        {
+            InputSimulator s = new InputSimulator();
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            count = 0;
+            Debug.WriteLine("수리가필요함");
+            s.Keyboard.KeyDown(VirtualKeyCode.RETURN);
+            Thread.Sleep(200);
+            Debug.WriteLine("엔터누름");
+            SendKeys.Send(fixCommnad);
+            Thread.Sleep(200);
+            s.Keyboard.KeyDown(VirtualKeyCode.RETURN);
+            //SendKeys.Send("{ENTER}");
+            Thread.Sleep(200);
+            Debug.WriteLine("돌조각 자동생성: " + FragmentAutoPower);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+
+            if (FragmentAutoPower == true)
+            {
+                Thread.Sleep(100);
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                s.Keyboard.KeyDown(VirtualKeyCode.VK_9);
+                Thread.Sleep(200);
+                mouse_event(MOUSEEVENTF_RBUTTONDOWN, 0, 0, 0, 0);
+                mouse_event(MOUSEEVENTF_RBUTTONUP, 0, 0, 0, 0);
+                Thread.Sleep(200);
+                mouse_event(MOUSEEVENTF_RBUTTONDOWN, 0, 0, 0, 0);
+                mouse_event(MOUSEEVENTF_RBUTTONUP, 0, 0, 0, 0);
+                Thread.Sleep(200);
+                mouse_event(MOUSEEVENTF_RBUTTONDOWN, 0, 0, 0, 0);
+                mouse_event(MOUSEEVENTF_RBUTTONUP, 0, 0, 0, 0);
+                Thread.Sleep(200);
+                mouse_event(MOUSEEVENTF_RBUTTONDOWN, 0, 0, 0, 0);
+                mouse_event(MOUSEEVENTF_RBUTTONUP, 0, 0, 0, 0);
+                Thread.Sleep(200);
+                mouse_event(MOUSEEVENTF_RBUTTONDOWN, 0, 0, 0, 0);
+                mouse_event(MOUSEEVENTF_RBUTTONUP, 0, 0, 0, 0);
+                Thread.Sleep(200);
+                mouse_event(MOUSEEVENTF_RBUTTONDOWN, 0, 0, 0, 0);
+                mouse_event(MOUSEEVENTF_RBUTTONUP, 0, 0, 0, 0);
+                Thread.Sleep(200);
+                mouse_event(MOUSEEVENTF_RBUTTONDOWN, 0, 0, 0, 0);
+                mouse_event(MOUSEEVENTF_RBUTTONUP, 0, 0, 0, 0);
+                Thread.Sleep(200);
+                //조약돌을 돌조각으로 바꿔주기 위하여 우클 연타
+
+                s.Keyboard.KeyDown(VirtualKeyCode.VK_1);
+
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            }
+        }
+
+        private void FixTool_Tick(object sender, EventArgs e)
+        {
+            Debug.WriteLine("타이머 진입");
+                count = count + 1;
+            if (FragmentAutoPower == true)
+            {
+                FragmentCount = FragmentCount + 1;
+            }
+            else
+            {
+                FragmentCount = 0;
+            }
+            Debug.WriteLine("카운트: " + count.ToString());
+            if (count == MiningLimit) {
+                Run();
+            }
+
+            if(leftPower == false)
+            {
+                FixToolTimer.Stop();
+                count = 0;
+                FragmentCount = 0;
+            }
+        }
+
+        private void FastClick_Tick(object sender, EventArgs e)
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            if(FastClickPower == false)
+            {
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                FastClickTimer.Stop();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("이 프로그램은 마인크래프트에서");
+            sb.AppendLine("자동으로 광질을 하거나,");
+            sb.AppendLine("좌클릭을 빠르게 하고 싶을때");
+            sb.AppendLine("사용하는 프로그램 입니다.");
+
+            FragmentCheckBox.Checked = false;
+
+            label3.Text = sb.ToString();
+
+
+            SetHook();
+
+            scanloopTimer = new System.Windows.Forms.Timer();
+            scanloopTimer.Interval = 50;
+            scanloopTimer.Tick += new EventHandler(scanLoop);
+
+            FixToolTimer.Interval = 1000; //주기 설정
+            FixToolTimer.Tick += new EventHandler(FixTool_Tick); //주기마다 실행되는 이벤트 등록
+
+            FastClickTimer = new System.Windows.Forms.Timer();
+            FastClickTimer.Interval = 10;
+            FastClickTimer.Tick += new EventHandler(FastClick_Tick);
+
+            IntPtr hWnd = FindWindow(null, AppPlayerName);
+            if(!hWnd.Equals(IntPtr.Zero))
+            {
+                Debug.WriteLine("윈도우 핸들 : " + hWnd);
+            }
+        }
+
+        public static IntPtr hookProc(int code, IntPtr wParam, IntPtr lParam)
+        {
+            if (code >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                if (vkCode.ToString() == "119")
+                {
+                    try
+                    {
+                        if (leftPower == false)
+                        {
+                            leftPower = true;
+                            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                            FixToolTimer.Start();
+                        }
+                        else
+                        {
+                            leftPower = false;
+                            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        return (IntPtr)MessageBox.Show("에러발생");
+                    }
+                    //Cursor.Position = new Point(242, 83);
+                }
+
+                if (vkCode.ToString() == "120")
+                {
+                    if(FastClickPower == false)
+                    {
+                        FastClickPower = true;
+                        FastClickTimer.Start();
+                    }
+                    else
+                    {
+                        FastClickPower = false;
+                    }
+                }
+
+                    //MessageBox.Show(vkCode.ToString()); 
+                    return CallNextHookEx(hhook, code, (int)wParam, lParam);
+            }
+            else
+                return CallNextHookEx(hhook, code, (int)wParam, lParam);
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void runNotice_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scanBtn_Click(object sender, EventArgs e)
+        {
+            scanloopTimer.Start();
+
+            IntPtr findwindow = FindWindow(null, AppPlayerName);
+
+            IntPtr fwEx = FindWindowEx(findwindow, 0, "GLFW30", "Minecraft 1.16.5 - 멀티플레이 (제삼자 서버)");
+            //1: findwindow 2: 0 3: 클래스명 4: 캡션명
+
+            if (findwindow != IntPtr.Zero)
+            {
+
+                //플레이어를 찾았을 경우
+                Debug.WriteLine("설정한 앱을 찾았습니다.");
+
+                try
+                {
+
+                    //찾은 플레이어를 바탕으로 Graphics 정보를 가져옵니다.
+                    Graphics Graphicsdata = Graphics.FromHwnd(findwindow);
+
+                    //찾은 플레이어 창 크기 및 위치를 가져옵니다. 
+                    Rectangle rect = Rectangle.Round(Graphicsdata.VisibleClipBounds);
+
+                    //플레이어 창 크기 만큼의 비트맵을 선언해줍니다.
+                    Bitmap bmp = new Bitmap(rect.Width, rect.Height);
+
+                    //비트맵을 바탕으로 그래픽스 함수로 선언해줍니다.
+                    using (Graphics g = Graphics.FromImage(bmp))
+                    {
+                        //찾은 플레이어의 크기만큼 화면을 캡쳐합니다.
+                        IntPtr hdc = g.GetHdc();
+                        PrintWindow(fwEx, hdc, 0x2);
+                        g.ReleaseHdc(hdc);
+                    }
+
+                    // pictureBox1 이미지를 표시해줍니다.
+                    pictureBox1.Image = bmp;
+                }
+                catch (Exception)
+                {
+                    Debug.WriteLine("오류");
+                }
+            }
+            else
+            {
+                //마크를 못찾을경우
+                Debug.WriteLine("설정한 앱을 찾지 못했습니다.");
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(FragmentCheckBox.Checked == false)
+            {
+                FragmentAutoPower = false;
+            }
+            else
+            {
+                FragmentAutoPower = true;
+            }
+
+        }
+
+        private void splitContainer1_Panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
